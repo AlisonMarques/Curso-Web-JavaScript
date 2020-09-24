0697f3bd1b4c3cdb61caf0f604870d7c49f41b1b
Alison Marques <alison-marques@outlook.com.br>
2020-09-24 16:13:32 -0300

    Entendendo o forOf

---
+++ esnext/set.js
@@ -0,0 +1,18 @@
+// não aceita repetição/não indexada
+const times = new Set();
+times.add('Vasco');
+// ou
+times.add('Sao paulo').add('Palmeiras').add('Corinthians');
+
+//testando repetição// só vai pegar o primeiro
+times.add('Vasco');
+
+console.log(times);
+
+console.log(times.has('vasco'));
+console.log(times.has('Vasco'));
+times.delete('Palmeiras');
+
+const nomes = ['Alison', 'Davi', 'Leia', 'Jeff'];
+const nomesSet = new Set(nomes);
+console.log(nomesSet);
